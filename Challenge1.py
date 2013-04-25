309211e82582869a85f4f804cd49d0d403af7858
TazyNTank <syn2192006@yahoo.com>
2013-04-25 17:47:55 -0500

    Updated credentials and server names

--- Challenge1.py
+++ Challenge1.py
@@ -1,14 +1,14 @@
 import pyrax
 import time
 import os
-pyrax.set_credential_file(os.path.expanduser("~/projects/pyrax.cfg"))
+pyrax.set_credential_file(os.path.expanduser("~/.rackspace_cloud_credentials")
 cs = pyrax.cloudservers
 flvs = cs.flavors.list()
 
 for x in range(0, 3):
 	
 	print "currently building server pyrax" + str(x)
-	server = cs.servers.create("Very Important Stuff" + str(x), "8bf22129-8483-462b-a020-1754ec822770", "2")
+	server = cs.servers.create("Web" + str(x), "8bf22129-8483-462b-a020-1754ec822770", "2")
  
 	print "Admin password:", server.adminPass
 
