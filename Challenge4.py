5bfa4fa33dfd4f20a804e42107ab85bb1bf57401
TazyNTank <syn2192006@yahoo.com>
2013-04-25 19:26:32 -0500

    Create A record when pass FQDN and IP

---
+++ Challenge4.py
@@ -0,0 +1,22 @@
+import pyrax
+import os
+import sys
+pyrax.set_credential_file(os.path.expanduser("~/.rackspace_cloud_credentials"))
+
+fqdn = sys.argv[1]
+ip = sys.argv[2]
+
+dns = pyrax.cloud_dns
+domainname = ".".join(fqdn.split(".")[-2:])
+
+print "Creating Domain"
+
+new_domain = dns.create(name=domainname, emailAddress="admin@example.com")
+
+print "Adding Records"
+
+new_domain.add_records({"type": "A", "name": fqdn, "data": ip, "ttl": "300"})
+
+for record in dns.list_records(new_domain):
+	print record.name
+	
